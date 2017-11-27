dc165b3c31926a59cdc6d43d15ac6d7fc20b2743
Codemupf <mupengfei6688@163.com>
2017-11-28 00:05:56 +0800

    更新LeetCode题目

--- src/Array/LeetCode621.java
+++ src/Array/LeetCode621.java
@@ -8,16 +8,19 @@ import java.util.Arrays;
  */
 public class LeetCode621 {
     public static int leastInterval(char[] tasks, int n) {
-
         int[] c = new int[26];
-        for(char t : tasks){
+        for (char t : tasks) {
             c[t - 'A']++;
         }
         Arrays.sort(c);
         int i = 25;
-        while(i >= 0 && c[i] == c[25]) i--;
-
+        while (i >= 0 && c[i] == c[25]) {
+            i--;
+        }
+//        放的下(c[25] - 1) * (n + 1) + 25 - i
+//        放不下tasks.length
         return Math.max(tasks.length, (c[25] - 1) * (n + 1) + 25 - i);
+
     }
 
     public static void main(String[] args) {
